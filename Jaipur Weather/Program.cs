38459a4762d1db714f3ba43bf685b463970212ce
Prateek Chaplot <prateek.chaplot@metacube.com>
2018-05-23 16:33:12 +0530

    - use MethodInvoker and BeginInvoke

--- Jaipur Weather/Program.cs
+++ Jaipur Weather/Program.cs
@@ -11,52 +11,52 @@ namespace Jaipur_Weather
     /// <summary>
     /// The main entry point for the application.
     /// </summary>
-    /// 
+    
     static class Program
     {
         private static JObject userData { get; set; }
-        //public static Object fileLock = new Object();
+        public static Object fileLock = new Object();
 
         static Form1 cls;
 
-        private static void SetData()
+        public static void SetData()
         {
-            string data = "";
+            lock (Program.fileLock)
+            {
+                string data = "";
 
-            //lock (Program.fileLock)
-            
                 using (WebClient wc = new WebClient())
                 {
                     data = wc.DownloadString("http://api.openweathermap.org/data/2.5/weather?id=1269515&units=metric&appid=4d302b00539441446f7736801e1bb1cc");
                 }
 
                 userData = JObject.Parse(data);
-            
+            }
+        }
+
+        public static void ShowData()
+        {
+            lock (Program.fileLock)
+            {
+                cls.chart1.Series["Temperature"].Points.AddXY("Jaipur", (int)userData["main"]["temp"]);
+                cls.chart1.Series["Pressure"].Points.AddXY("Jaipur", (int)userData["main"]["pressure"]);
+                cls.chart1.Series["Humidity"].Points.AddXY("Jaipur", (int)userData["main"]["humidity"]);
+            }
         }
 
         public static void Set()
         {
             var startTimeSpan = TimeSpan.Zero;
-            var periodTimeSpan = TimeSpan.FromSeconds(30);
+            var periodTimeSpan = TimeSpan.FromSeconds(10);
 
             var timer = new System.Threading.Timer((e) =>
             { Program.SetData(); }, null, startTimeSpan, periodTimeSpan);
         }
-
-        private static void ShowData()
-        {
-            //lock (Program.fileLock)
-            
-                cls.chart1.Series["Temperature"].Points.AddXY("Jaipur", (int)userData["main"]["temp"]);
-                cls.chart1.Series["Pressure"].Points.AddXY("Jaipur", (int)userData["main"]["pressure"]);
-                cls.chart1.Series["Humidity"].Points.AddXY("Jaipur", (int)userData["main"]["humidity"]);
-            
-        }
-
+        
         public static void Show()
         {
             var startTimeSpan = TimeSpan.Zero;
-            var periodTimeSpan = TimeSpan.FromMinutes(1);
+            var periodTimeSpan = TimeSpan.FromSeconds(90);
 
             var timer = new System.Threading.Timer((e) =>
             { Program.ShowData(); }, null, startTimeSpan, periodTimeSpan);

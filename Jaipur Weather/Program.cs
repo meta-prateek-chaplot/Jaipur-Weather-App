cbbbea439c6a12855925813e2633bd7eefca215c
Prateek Chaplot <prateek.chaplot@metacube.com>
2018-05-24 14:29:25 +0530

    - removed unnecessary namespaces

--- Jaipur Weather/Program.cs
+++ Jaipur Weather/Program.cs
@@ -1,17 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Newtonsoft.Json.Linq;
 
 namespace Jaipur_Weather
 {
     /// <summary>
     /// The main entry point for the application.
     /// </summary>
-    
+
     static class Program
     {
         [STAThread]
